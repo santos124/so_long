f846659bf7c1149e874605c1d4d0f96b4cc7972e
Waltyr Adina <wadina@at-h3.msk.21-school.ru>
2021-10-23 15:54:01 +0300

    a

--- SRC/main.c
+++ SRC/main.c
@@ -1,15 +1,32 @@
 #include <mlx.h>
 
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
 int	main(void)
 {
 	void	*mlx;
-	void	*img;
-	char	*relative_path = "./test.xpm";
-	int		img_width;
-	int		img_height;
+	void	*mlx_win;
+	t_data	img;
 
 	mlx = mlx_init();
-	img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
-	while (1)
-		;
+	mlx_win = mlx_new_window(mlx, 800, 600, "Hello world!");
+	img.img = mlx_new_image(mlx, 800, 600);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
 }
